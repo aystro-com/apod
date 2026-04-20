76daaa63b9d3b61f5d03c16bfac782bda8892560
Muntaser AbuZaid <montaserabuzaid@gmail.com>
2026-04-20 22:27:56 +0300

    fix: remove main.go version override that clobbered goreleaser ldflags

--- cmd/apod/main.go
+++ cmd/apod/main.go
@@ -4,13 +4,9 @@ import (
 	"os"
 
 	"github.com/aystro/apod/internal/cli"
-	"github.com/aystro/apod/internal/engine"
 )
 
-var version = "dev"
-
 func main() {
-	engine.Version = version
 	if err := cli.Execute(); err != nil {
 		os.Exit(1)
 	}
